91667a764385cfcee836931f4ddeea93ee947a34
sahilsao <sahilsaocs@gmail.com>
2025-05-30 17:19:32 +0530

    fix

---
+++ Northwind.Client/Sessions/CustomerSessions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+
+namespace NorthwindDemo.Sessions
+{
+    public class CustomerSessions
+    {
+        public readonly ProtectedSessionStorage ProtectedSessionStorage;
+        public CustomerSessions(ProtectedSessionStorage _ProtectedSessionStore)
+        {
+            ProtectedSessionStorage = _ProtectedSessionStore;
+        }
+
+        public async Task<String> GetCustomerID()
+        {
+            var val = await ProtectedSessionStorage.GetAsync<String>("CustomerID");
+            return val.Value!;
+        }
+
+        public async Task<String> GetCompanyName()
+        {
+            var val = await ProtectedSessionStorage.GetAsync<String>("CompanyName");
+            return val.Value!;
+        }
+
+        public async Task RemoveSessions()
+        {
+            await ProtectedSessionStorage.DeleteAsync("CustomerID");
+            await ProtectedSessionStorage.DeleteAsync("CompanyName");
+        }
+    }
+}
